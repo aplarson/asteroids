bcab08ad1a41b0a097102355404d16414b57893c
Andrew P. Larson <andrew.p.larson@gmail.com>
2014-12-19 14:02:57 -0800

    Shrink bullets

--- bullet.js
+++ bullet.js
@@ -1,19 +1,19 @@
 (function () {
 
-if (typeof Asteroids === "undefined") {
-  window.Asteroids = {};
-}
+  if (typeof Asteroids === "undefined") {
+    window.Asteroids = {};
+  }
 
-var Bullet = Asteroids.Bullet = function (pos, game, vel) {
-  Asteroids.MovingObject.call(this, { pos: pos,
-                                      vel: vel,
-                                      radius: Asteroids.Bullet.RADIUS,
-                                      color: Asteroids.Util.randomColor(),
-                                      game: game });
-  this.isWrappable = false;
-};
+  var Bullet = Asteroids.Bullet = function (pos, game, vel) {
+    Asteroids.MovingObject.call(this, { pos: pos,
+                                        vel: vel,
+                                        radius: Asteroids.Bullet.RADIUS,
+                                        color: Asteroids.Util.randomColor(),
+                                        game: game });
+    this.isWrappable = false;
+  };
 
-Asteroids.Util.inherits(Bullet, Asteroids.MovingObject);
+  Asteroids.Util.inherits(Bullet, Asteroids.MovingObject);
 
-Bullet.RADIUS = 10;
+  Bullet.RADIUS = 5;
 })();
